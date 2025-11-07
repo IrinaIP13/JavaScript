ffe465e20f6cbf9956aa696545b5842cf7610196
IrinaIP13 <irina.ip1303@gmail.com>
2025-11-07 23:14:51 +0200

    HW4

---
+++ HW-4/hw4-13/main.js
@@ -0,0 +1,15 @@
+// #mkGDenYnNjn.html
+//
+// Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+//
+// Приклад exchange(10000,[{currency:’USD’,value:25},{currency:’EUR’,value:42}],’USD’) // => 400
+
+function exchange(sumUAH, currencyValues, exchangeCurrency) {
+    let value = 0;
+    for (const obj of currencyValues) {
+        if (obj.currency === exchangeCurrency) value = obj.value;
+    }
+    return value * sumUAH;
+}
+
+console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
