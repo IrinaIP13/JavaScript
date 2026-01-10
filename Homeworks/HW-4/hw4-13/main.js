11198b355d83c1f8bb2552ebb64d3af4bed47a0f
IrinaIP13 <irina.ip1303@gmail.com>
2026-01-10 14:11:56 +0200

    Correct

--- Homeworks/HW-4/hw4-13/main.js
+++ Homeworks/HW-4/hw4-13/main.js
@@ -9,7 +9,9 @@ function exchange(sumUAH, currencyValues, exchangeCurrency) {
     for (const obj of currencyValues) {
         if (obj.currency === exchangeCurrency) value = obj.value;
     }
-    return value * sumUAH;
+    if (value === 0) {
+        return sumUAH/value;
+    } else return -1;
 }
 
 console.log(exchange(10000, [{currency: 'USD', value: 25}, {currency: 'EUR', value: 42}], 'USD'));
